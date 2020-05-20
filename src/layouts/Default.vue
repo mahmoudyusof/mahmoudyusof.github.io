553ddc24ce917194e4e33fd74a4839baf4a0d0b2
mahmoudyusof <mahmoud.yusof27@gmail.com>
2020-05-20 22:00:24 +0200

    fixing footer and adding tooltips

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,38 +3,62 @@
     <v-app-bar color="primary" fixed elevate-on-scroll>
       <v-toolbar-title>SembaSource</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        @click="$vuetify.goTo('#projects', {duration: 300, offset: 100})"
-        class="nav-link primary"
-        elevation="0"
-        icon
-      >
-        <v-icon>home</v-icon>
-      </v-btn>
-      <v-btn
-        @click="$vuetify.goTo('#about', {duration: 300, offset: 100})"
-        class="nav-link primary"
-        elevation="0"
-        icon
-      >
-        <v-icon>help_outline</v-icon>
-      </v-btn>
-      <v-btn
-        @click="$vuetify.goTo('#contact', {duration: 300, offset: 100})"
-        class="nav-link primary"
-        elevation="0"
-        icon
-      >
-        <v-icon>mail_outline</v-icon>
-      </v-btn>
-      <v-btn
-        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
-        icon
-        elevation="0"
-        class="nav-link primary"
-      >
-        <v-icon>invert_colors</v-icon>
-      </v-btn>
+      <v-tooltip bottom>
+        <template v-slot:activator="{on}">
+          <v-btn
+            @click="$vuetify.goTo('#projects', {duration: 300, offset: 100})"
+            class="nav-link primary"
+            elevation="0"
+            icon
+            v-on="on"
+          >
+            <v-icon>home</v-icon>
+          </v-btn>
+        </template>
+        <span>Home</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{on}">
+          <v-btn
+            @click="$vuetify.goTo('#about', {duration: 300, offset: 100})"
+            class="nav-link primary"
+            elevation="0"
+            icon
+            v-on="on"
+          >
+            <v-icon>help_outline</v-icon>
+          </v-btn>
+        </template>
+        <span>About</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{on}">
+          <v-btn
+            @click="$vuetify.goTo('#contact', {duration: 300, offset: 100})"
+            class="nav-link primary"
+            elevation="0"
+            icon
+            v-on="on"
+          >
+            <v-icon>mail_outline</v-icon>
+          </v-btn>
+        </template>
+        <span>Contact</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{on}">
+          <v-btn
+            @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+            icon
+            elevation="0"
+            class="nav-link primary"
+            v-on="on"
+          >
+            <v-icon>invert_colors</v-icon>
+          </v-btn>
+        </template>
+        <span v-text="$vuetify.theme.dark ? 'Light' : 'Dark'"></span>
+      </v-tooltip>
     </v-app-bar>
     <v-container fluid color="primary" class="dark-container">
       <slot />
@@ -43,10 +67,10 @@
       <v-container>
         <v-row>
           <v-col cols="12" sm="4">
-            <v-list class="primary" dense>
+            <v-list flat class="primary" dense>
               <v-subheader>Social media</v-subheader>
-              <v-list-item-group color="primary">
-                <v-list-item>
+              <v-list-item-group color="white">
+                <v-list-item href="https://www.facebook.com/mahmoud.youssef27/" target="_blank">
                   <v-list-item-icon>
                     <v-icon>fab fa-facebook</v-icon>
                   </v-list-item-icon>
@@ -55,7 +79,10 @@
                   </v-list-item-content>
                 </v-list-item>
 
-                <v-list-item>
+                <v-list-item
+                  href="https://www.linkedin.com/in/mahmoud-yusof-a39ab5151/"
+                  target="_blank"
+                >
                   <v-list-item-icon>
                     <v-icon>fab fa-linkedin</v-icon>
                   </v-list-item-icon>
@@ -64,22 +91,22 @@
                   </v-list-item-content>
                 </v-list-item>
 
-                <v-list-item>
+                <v-list-item href="https://twitter.com/mahmoudyusof" target="_blank">
                   <v-list-item-icon>
-                    <v-icon>fab fa-github</v-icon>
+                    <v-icon>fab fa-twitter</v-icon>
                   </v-list-item-icon>
                   <v-list-item-content>
-                    <v-list-item-title>Github</v-list-item-title>
+                    <v-list-item-title>@mahmoudyusof</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
               </v-list-item-group>
             </v-list>
           </v-col>
           <v-col cols="12" sm="4">
-            <v-list class="primary" dense>
+            <v-list flat class="primary" dense>
               <v-subheader>Contact Information</v-subheader>
-              <v-list-item-group color="primary">
-                <v-list-item>
+              <v-list-item-group color="white">
+                <v-list-item href="mailto:mahmoud.yusof27@gmail.com">
                   <v-list-item-icon>
                     <v-icon>mail_outline</v-icon>
                   </v-list-item-icon>
@@ -96,44 +123,35 @@
                     <v-list-item-title>+20 111 601 0514</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon>fab fa-twitter</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title>@mahmoudyusof</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
               </v-list-item-group>
             </v-list>
           </v-col>
           <v-col cols="12" sm="4">
-            <v-list class="primary" dense>
+            <v-list flat class="primary" dense>
               <v-subheader>Credits</v-subheader>
-              <v-list-item-group color="primary">
-                <v-list-item>
+              <v-list-item-group color="white">
+                <v-list-item href="https://unsplash.com/@brenomachado" target="_blank">
                   <v-list-item-icon>
                     <v-icon>image</v-icon>
                   </v-list-item-icon>
                   <v-list-item-content>
-                    <v-list-item-title>https://unsplash.com/@brenomachado</v-list-item-title>
+                    <v-list-item-title>@brenomachado</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
 
-                <v-list-item>
+                <v-list-item href="https://unsplash.com/@epicantus" target="_blank">
                   <v-list-item-icon>
                     <v-icon>image</v-icon>
                   </v-list-item-icon>
                   <v-list-item-content>
-                    <v-list-item-title>https://unsplash.com/@epicantus</v-list-item-title>
+                    <v-list-item-title>@epicantus</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
               </v-list-item-group>
             </v-list>
           </v-col>
         </v-row>
-        <v-row>
+        <v-row class="px-5">
           <p class="copy">Copy rights &copy; 2020, Designed and created by Mahmoud Youssef</p>
         </v-row>
       </v-container>
